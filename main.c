49fb2d09b80c1de31d39b611bdebaabd269e07a4
Unknown <pablo2018q2@spamhereplease.com>
2018-08-08 11:54:11 -0300

    agregado comentarios

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+/*
+Este programa imprime hola hola en pantalla.
+
+Grupo 6
+*/
+
 #include <stdio.h>
 
 int main (void)
 {
+    //Imprimo hola en pantalla
     printf("Hola hola!");
+
+    //Fin de programa
     return 0;
 }
